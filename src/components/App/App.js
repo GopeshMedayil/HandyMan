9f0367a0bb50723b495d80987a5fb3d8b8fb088d
GopeshMedayil <medayilgopesh@gmail.com>
2018-11-13 22:32:55 +0530

    Added Modal component and add project form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 //@flow
 import React, { PureComponent, Fragment } from "react";
-import Modal from '../Modal/Modal';
+import Modal from '../Modal';
 
 type Props = {
+    onClose: () => void
 
-}
+};
 type State = {
     show: boolean;
-}
+};
 
 class App extends PureComponent<Props, State> {
 
@@ -15,10 +16,17 @@ class App extends PureComponent<Props, State> {
         show: false
     }
     showModal = () => {
+
         this.setState({
             ...this.state,
             show: !this.state.show
-        })
+        });
+
+    }
+
+    onSubmit = (e: any) => {
+        e.preventDefault();
+        alert("submit")
     }
 
     componentDidMount() {
@@ -30,8 +38,22 @@ class App extends PureComponent<Props, State> {
             <Fragment>
                 <div className="well"><p>Welcome Handyman!!</p></div>
                 <button className="btn btn-primary btn-small" onClick={this.showModal}>Add Project</button>
-                <Modal show={this.state.show}>
-                    THis is a Modal component
+                <Modal show={this.state.show} onClose={this.showModal}>
+                    <h2>Add Project</h2>
+                    <hr />
+                    <form>
+
+                        <div className="form-group">
+                            <label htmlFor="upldpjct">Upload Project:</label>
+                            <button className="btn btn-secondary">Choose Project</button>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="port">Port:</label>
+                            <input type="text" className="form-control" id="port" />
+                        </div>
+
+                        <button type="submit" className="btn btn-default" onClick={e => this.onSubmit(e)}>Submit</button>
+                    </form>
                 </Modal>
             </Fragment>
 
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,14 +1,65 @@
 import React, { Component } from 'react';
 
+const backDropStlye = {
+    position: 'fixed',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    padding: 50
+}
+const modalStyle = {
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    maxWidth: 800,
+    minHeight: 300,
+    margin: '0 auto',
+    padding: 30,
+    position: "relative"
+};
+
+const footerStyle = {
+    position: "absolute",
+    bottom: 20
+};
+
 export default class Modal extends Component {
+
+    onClose = () => {
+        this.props.onClose && this.props.onClose();
+    }
+    onKeyUp = (e) => {
+        // Lookout for ESC key (27)
+        if (e.which === 27 && this.props.show) {
+            this.onClose(e);
+        }
+    }
+    componentDidMount = () => {
+        console.log("DID MOUNT")
+        document.addEventListener("keyup", this.onKeyUp);
+    }
+
+    componentWillUnmount = () => {
+        console.log("UNMOUNT");
+        document.removeEventListener("keyup", this.onKeyUp);
+
+    }
     render() {
         if (!this.props.show) {
             return null;
         }
         return (
-            <div>
-                <h2>Welcome Modal</h2>
-                {this.props.children}
+            <div style={backDropStlye}>
+                <div style={modalStyle}>
+                    <div style={footerStyle}>
+                        <button onClick={(e) => { this.onClose(e) }}>
+                            Close
+                        </button>
+                    </div>
+                    {this.props.children}
+
+                </div>
             </div>
 
         )
